da929d7b1968660e54a5842b5474534158a309bd
Neeraj Kumar <neeraj@flick2know.com>
2024-02-18 00:33:16 +0530

    feat(module): add @nuxtjs/tailwindcss module

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
-})
+  devtools: { enabled: true },
+  ssr: false,
+  devServer: {
+    port: 10023,
+  },
+  modules: ["@nuxtjs/tailwindcss"],
+});
---
+++ pages/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <h1 class="bg-red-500">Home</h1>
+    <p>My name is {{ name }}</p>
+    <button @click="changeName">Change name</button>
+    <p>This is the {{ message }}</p>
+  </div>
+</template>
+
+<script lang="ts">
+import BaseMixin from "~/mixins/BaseMixin";
+
+export default defineComponent({
+  mixins: [BaseMixin],
+  data() {
+    return {
+      name: "Nuxt.jssss",
+    };
+  },
+
+  methods: {
+    changeName() {
+      this.name = "Nuxt.js";
+    },
+  },
+});
+</script>
